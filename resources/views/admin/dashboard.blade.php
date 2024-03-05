e85a1cbfe377a4ef3ef7631e7c20d70f459693f4
Salma <salmaelgmiri04@gmail.com>
2024-03-05 09:32:13 +0100

    add Authentification & CRUD category

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,166 @@
+
+@include('admin.header')
+   
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card  mb-2">
+                            <div class="card-header p-3 pt-2">
+                                <div
+                                    class="icon icon-lg icon-shape shadow-dark shadow text-center border-radius-xl mt-n4 position-absolute" style="background-color: #787d62">
+                                    <i class="material-icons opacity-10">weekend</i>
+                                </div>
+                                <div class="text-end pt-1">
+                                    <b class="text-sm mb-0 text-capitalize text-primary">Totale Books</b>
+                                    <h4 class="mb-0"> </h4>
+                                </div>
+                            </div>
+
+                            <hr class="dark horizontal my-0">
+                            <div class="card-footer p-3">
+                                <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than
+                                    last week</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card  mb-2">
+                            <div class="card-header p-3 pt-2">
+                                <div
+                                    class="icon icon-lg icon-shape  shadow text-center border-radius-xl mt-n4 position-absolute" style="background-color: #FFDAB9	">
+                                    <i class="material-icons opacity-10">leaderboard</i>
+                                </div>
+                                <div class="text-end pt-1">
+                                    <b class="text-sm mb-0 text-capitalize text-primary">Today's Users</b >
+                                    <h4 class="mb-0"></h4>
+                                </div>
+                            </div>
+
+                            <hr class="dark horizontal my-0">
+                            <div class="card-footer p-3">
+                                <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+3% </span>than
+                                    last month</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card  mb-2">
+                            <div class="card-header p-3 pt-2 bg-transparent">
+                                <div
+                                    class="icon icon-lg icon-shape  shadow-success text-center border-radius-xl mt-n4 position-absolute"  style="background-color: #D2B48C		">
+                                    <i class="material-icons opacity-10">store</i>
+                                </div>
+                                <div class="text-end pt-1">
+                                    <b class="text-sm mb-0 text-capitalize text-primary ">All The Copies</b>
+                                    <h4 class="mb-0 "></h4>
+                                </div>
+                            </div>
+    
+                            <hr class="horizontal my-0 dark">
+                            <div class="card-footer p-3">
+                                <p class="mb-0 "><span class="text-success text-sm font-weight-bolder">+1% </span>than
+                                    yesterday</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card mb-2">
+                            <div class="card-header p-3 pt-2 bg-transparent">
+                                <div
+                                    class="icon icon-lg icon-shape  shadow-info text-center border-radius-xl mt-n4 position-absolute" style="background-color: #EEE8AA	">
+                                    <i class="material-icons opacity-10">person_add</i>
+                                </div>
+                                <div class="text-end pt-1">
+                                    <b class="text-sm mb-0 text-capitalize text-primary ">Available Copies</b>
+                                    <h4 class="mb-0 "></h4>
+                                </div>
+                            </div>
+    
+                            <hr class="horizontal my-0 dark">
+                            <div class="card-footer p-3">
+                                <p class="mb-0 ">Just updated</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     
+    
+
+        <div class="fresh-table full-color-orange">
+  <!--
+    Available colors for the full background: full-color-blue, full-color-azure, full-color-green, full-color-red, full-color-orange
+    Available colors only for the toolbar: toolbar-color-blue, toolbar-color-azure, toolbar-color-green, toolbar-color-red, toolbar-color-orange
+  -->
+
+  
+    <!-- <div class="col-lg-3 col-sm-6 col-12 mt-3 mb-3">
+                  <button class="btn bg-gradient-success w-50 mb-0 toast-btn" type="button" data-target="successToast"><a href="form.php">ADD Client</a></button>
+                </div> -->
+{{--   
+
+  <table id="fresh-table" class="table">
+    <thead>
+      <th data-field="id">ID</th>
+      <th data-field="name">Fullname</th>
+      <th data-field="Email">Email</th>
+      <th data-field="Role">Role</th>
+      <th data-field="Role">Actions</th>
+      
+    </thead>
+    <tbody>
+   
+    <tr>
+        <td>{{ $user->id }}</td>
+        <td>{{ $user->name}}</td>
+        <td>{{ $user->email }}</td>
+        <td> @foreach ($user->roles as $role)
+            {{ $role->name }}
+        @endforeach</td>
+              <!-- <td><button  class="btn btn-default"><a href="edit.php?id=">Edit</a></button></td>-->
+              <td>
+                <form action="{{ route('users.edit',$user->id) }}" >
+                @method('PUT')
+                @csrf
+                
+                <button type="submit"class="btn btn-default"><i class="material-icons text-sm me-2">edit</i></button>
+               </form>
+            </td> 
+              <td>
+                <form action="{{ route('users.destroy',$user->id)}}" method="post">
+                @method('DELETE')
+                @csrf
+                <button  class="btn btn-default"><lord-icon
+                    src="https://cdn.lordicon.com/skkahier.json"
+                    trigger="hover"
+                    style="width:30px;height:30px">
+                </lord-ico></button>
+            </form>
+        </td>
+    </tr> 
+    @endforeach
+ 
+  </div>
+      
+     
+    
+    </tbody>
+  </table> --}}
+</div>
+
+    </div>
+
+
+    
+    
+
+   
+<script src="https://cdn.lordicon.com/lordicon.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,59 @@
+
+  <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   " id="sidenav-main" style="background-color: #54a054b1">
+    <div class="sidenav-header">
+      <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
+        <img src="{{ asset('images/EVENTO.svg')}}" class="navbar-brand-img h-200 w-50" alt="main_logo">
+       
+      </a>
+    </div>
+    <hr class="horizontal light mt-0 mb-2">
+    <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link text-white sidebar " href="">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">dashboard</i>
+            </div>
+            <span class="nav-link-text ms-1">All Users</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white " href="">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">table_view</i>
+            </div>
+            <span class="nav-link-text ms-1">Arts Projects</span>
+          </a>
+        </li>
+        <li class="nav-item">
+  <a class="nav-link text-white " href="partner">
+    
+      <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+        <i class="material-icons opacity-10">assignment</i>
+      </div>
+    
+    <span class="nav-link-text ms-1">Partners</span>
+  </a>
+
+</li>
+<li class="nav-item">
+  <a class="nav-link text-white " href="">
+    
+      <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+        <i class="material-icons opacity-10">assignment</i>
+      </div>
+    
+    <span class="nav-link-text ms-1">Collaborations</span>
+  </a>
+  
+</li>
+      </ul>
+    </div>
+    <div class="sidenav-footer position-absolute w-100 bottom-0 ">
+      <div class="mx-3">
+        <a class="btn sidebar mt-4 w-100" href="#" type="button" >Upgrade to pro</a>
+      </div>
+    </div>
+  </aside>
+
